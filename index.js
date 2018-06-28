3b415e44b8498b6ff69c1d4e83b7c9359298ff12
coolguzman11 <37478472+coolguzman11@users.noreply.github.com>
2018-06-28 16:30:23 -0500

    Update index.js

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ client.on("message", message => {
       console.log(`SUCCESS!: ${JSON.stringify(data)}`);
   
       message.delete().catch();
-      async message.reply("SUCCESS! Patrol Log Posted!").then(msg => {msg.delete(9000)})
+      message.reply("SUCCESS! Patrol Log Posted!").then(msg => {msg.delete(9000)})
       
     }).catch((err) => {
       console.log(`FAILED!: ${err}`);
