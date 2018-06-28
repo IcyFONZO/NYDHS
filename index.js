62ff8d2453f0e0f78082d63ab92ecd8f8c59a8fc
coolguzman11 <37478472+coolguzman11@users.noreply.github.com>
2018-06-28 17:15:13 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,9 +6,8 @@ const trelloToken = process.env.TRELLO_TOKEN;
 const discordBotToken = process.env.DISCORD_BOT_TOKEN;
 const discordChannelID = process.env.DISCORD_CHANNEL_ID;
 const trelloIDList = process.env.TRELLO_ID_LIST_SS;
-const trelloIDList2 = process.env.TRELLO_ID_LIST_SRT;
 
-[trelloKey, trelloToken, discordBotToken, discordChannelID, trelloIDList, trelloIDList2].forEach(i => {
+[trelloKey, trelloToken, discordBotToken, discordChannelID, trelloIDList].forEach(i => {
   if (!i) {
     console.log("Token is undefined. Please set .env file. Exit...");
     process.exit(0);
@@ -29,8 +28,6 @@ client.on("message", message => {
     if(message.author.bot) return;
   if (message.channel.id === discordChannelID) {
    
-   if(message.content == 'SS') {
-
     console.log("gatcha!");
 
     postTrello(trelloClient, trelloIDList, message).then((data) => {
@@ -41,25 +38,7 @@ client.on("message", message => {
       
     }).catch((err) => {
       console.log(`FAILED!: ${err}`);
-    }
-    
-    
-    if(message.author.bot) return;
-  if (message.channel.id === discordChannelID) {
-   
-   if(message.content == 'SRT') {
-
-    console.log("gatcha!");
-
-    postTrello(trelloClient, trelloIDList2, message).then((data) => {
-      console.log(`SUCCESS!: ${JSON.stringify(data)}`);
-  
-      message.delete().catch();
-      message.reply("SUCCESS! Patrol Log Posted!").then(msg => {msg.delete(9000)})
-      
-    }).catch((err) => {
-      console.log(`FAILED!: ${err}`);
-    })};
+    });
   }
 });
 
