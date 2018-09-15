c3fc29702a06ba2b5872e97c692c55ff055f29ba
coolguzman11 <37478472+coolguzman11@users.noreply.github.com>
2018-09-15 16:15:00 -0500

    Update index.js

--- index.js
+++ index.js
@@ -66,7 +66,7 @@ client.on("message", message => {
       let raskj6 = new discord.RichEmbed()
       .setTitle("Patrol Logs Format")
       .setDescription("The following format **MUST** be followed in-order for your Patrol Log to be logged into the Trello.")
-      .addField("Division: \n Username: \n Patrol Screenshot: \n Date: \n Start Time: \n End Time:", "Keep in mind, division abbreviations **MUST** be used. [SS, FPS, SRT, HSI] Links will **ONLY** be accepted for patrol screenshots. The format only has to be in the order provided, capitalization will not affect the logging process.")
+      .addField(" Division: \n Username: \n Patrol Screenshot: \n Date: \n Start Time: \n End Time:", " Keep in mind, division abbreviations **MUST** be used. [SS, FPS, SRT, HSI] \n Links will **ONLY** be accepted for patrol screenshots. \n The format only has to be in the order provided, capitalization will not affect the logging process.")
       .setColor("#ccccdd")
       .setThumbnail("https://cdn.discordapp.com/attachments/462447883849957397/462653415990755339/download.png")
 
